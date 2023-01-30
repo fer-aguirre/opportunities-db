b964e4d37ee7ff1011b3a5f7128524b1d14f3330
Fer Aguirre <kay_op@protonmail.com>
2023-01-30 17:43:38 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Opportunities Database',
+    version='0.1.0',
+    author='Fer Aguirre',
+    description='A template for converting websites related to opportunities (e.g. funds, fellowships, etc.) to structured data.',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
