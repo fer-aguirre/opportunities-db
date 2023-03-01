1201df490e14bba07e842cd0df26d2b21ff88224
Fer Aguirre <kay_op@protonmail.com>
2023-03-01 15:53:58 -0600

    Add functions to export data

---
+++ opportunities_db/data/export.py
@@ -0,0 +1,12 @@
+import pandas as pd
+# Import modules
+import opportunities_db.data.load as load
+
+# Load data
+data_processed = load.data_processed
+
+def update_data(df1, df2):
+    # Join dataframes
+    df_join = df1.merge(df2, how='outer')
+    # Save dataframe as 'csv' file
+    df_join.to_csv(data_processed, index=False)
