2aa9135f52deedfe3030eca3ec6180ceb0c98aa0
Fer Aguirre <kay_op@protonmail.com>
2023-03-01 22:10:52 -0600

    Update file to process data

--- process-data.py
+++ process-data.py
@@ -2,8 +2,6 @@
 import opportunities_db.data.load as load
 import opportunities_db.data.analyze as analyze
 import opportunities_db.data.export as export
-
-# Import libraries
 import logging
 
 
@@ -19,25 +17,33 @@ def main():
 
     # Process data
     diff = analyze.compare_data(df1, df2)
+
     # If there's new data
     if len(diff) != 0:
+        # Create empty lists
+        texts = []
+        titles = []
+        deadlines = []
+        summaries = []
         try:
-            # Create empty lists
-            texts = []
-            titles = []
-            deadlines = []
-            summaries = []
             # Iterate over the list of urls
             for i in range(len(diff)):
+
+                # Make HTTP request
+                soup = analyze.make_request(diff[i])
+
                 # Extract the text
-                text = analyze.extract_text(diff[i])
+                text = analyze.extract_text(soup)
                 texts.append(text)
+
                 # Extract the title
-                title = analyze.extract_title(diff[i])
+                title = analyze.extract_title(soup)
                 titles.append(title)
+
                 # Extract the deadline
                 deadline = analyze.extract_deadline(diff[i])
                 deadlines.append(deadline)
+
                 # Extact a summary
                 summary = analyze.extract_summary(diff[i])
                 summaries.append(summary)
@@ -45,10 +51,12 @@ def main():
             # Create a dataframe from lists
             df_new = analyze.create_dataframe(titles, deadlines, summaries, diff)
             export.update_data(df1, df_new)
+
             # Update log file
             logging.basicConfig(filename="log.txt", level=logging.INFO,
                                 format="%(asctime)s %(message)s")
             logging.info("Database updated")
+            
         except Exception:
             logging.basicConfig(filename="log.txt", level=logging.ERROR,
                     format="%(asctime)s %(message)s")
