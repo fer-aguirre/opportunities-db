c5e486798a5678444513b61857e500bb9ee5b898
Fer Aguirre <kay_op@protonmail.com>
2023-03-05 23:39:19 -0600

    Create new functions to export data

--- opportunities_db/data/export.py
+++ opportunities_db/data/export.py
@@ -5,10 +5,27 @@ import opportunities_db.data.load as load
 
 # Load data
 data_processed = load.data_processed
+last_version = load.last_version
 
 
-def update_data(df1, df2):
+def update_data(df):
+    """
+    Function to save dataframe as csv file
+
+    df: pandas.DataFrame
+    """
+    # Save dataframe as 'csv' file
+    df.to_csv(data_processed, index=False)
+
+
+def update_last_version(df1, df2):
+    """
+    Function to merge two dataframes and save as csv file
+
+    df1: pandas.DataFrame
+    df2: pandas.DataFrame
+    """
     # Join dataframes
     df_join = df1.merge(df2, how="outer")
     # Save dataframe as 'csv' file
-    df_join.to_csv(data_processed, index=False)
+    df_join.to_csv(last_version, index=False)
--- opportunities_db/data/load.py
+++ opportunities_db/data/load.py
@@ -3,8 +3,10 @@ import opportunities_db.utils.paths as paths
 
 # Inputs
 data_raw = paths.data_raw_dir("opportunities_urls.csv")
-new_version = paths.assets_dir("new_version.csv")
+new_urls = paths.assets_dir("new_urls.csv")
+data_processed = paths.data_processed_dir("opportunities_db.csv")
+last_version = paths.data_processed_dir("last_version.csv")
 
 # Outputs
-data_processed = paths.data_processed_dir("opportunities_db.csv")
+data_proc_dir = paths.data_processed_dir()
 sqlite = paths.data_processed_dir("opportunities.db")
